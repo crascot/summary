3c749a80f31b4f657571b078415591995f90508a
Yaroslav <yarsitdikov@gmail.com>
2025-09-25 15:44:22 +0600

    some fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,20 @@
 import "./Header.less";
 import MailIcon from "../../icons/mail.svg";
-// import PhoneIcon from "../../icons/phone.svg";
+import { useParams } from "react-router-dom";
 import SchoolIcon from "../../icons/school.svg";
 import GitHubIcon from "../../icons/github.svg";
 import TelegramIcon from "../../icons/telegram.svg";
 import DownloadIcon from "../../icons/download.svg";
 import GlobeIcon from "../../icons/globe.svg";
+import LocationIcon from "../../icons/location.svg";
 import { Link } from "../Link";
 import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
 import {
  EMAIL_LINK,
  GITHUB_LINK,
- PDF_LINK,
- //  PHONE_LINK,
  TELEGRAM_LINK,
  DIPLOMA_LINK,
+ PDF_LINK,
 } from "./consts";
 
 export const Header = () => {
@@ -27,33 +26,54 @@ export const Header = () => {
    <div className="header-block">
     <div className="header-block-info">
      <h1>{t("name")}</h1>
-     <div className="header-block-info-links">
+     <div className="header-block-info">
+      <div className="header-block-info-section">
+       <h4>{t("information.contacts")}</h4>
+       <span>
+        <LocationIcon />
+        <span>{t("information.city")}</span>
+       </span>
+       <span>
+        <MailIcon />
+        <Link href={EMAIL_LINK} text="yarsitdikov@gmail.com" />
+       </span>
+       <span>
+        <TelegramIcon />
+        <Link href={TELEGRAM_LINK} text="Telegram" />
+       </span>
+       <span>
+        <GitHubIcon />
+        <Link href={GITHUB_LINK} text="GitHub" />
+       </span>
+      </div>
+
+      <div className="header-block-info-section">
+       <h4>{t("information.resumeAndDiploma")}</h4>
+       <span>
+        <SchoolIcon />
+        <Link href={DIPLOMA_LINK} text={t("university")} />
+       </span>
+      </div>
+
+      <div className="header-block-info-section">
+       <h4>{t("information.languages")}</h4>
+       <span className="header-block-info-section-languages">
+        <GlobeIcon />
+        <ul>
+         {(t("languages.compact", { returnObjects: true }) as string[]).map(
+          (lan: string, i: number) => (
+           <li key={i}>{lan}</li>
+          ),
+         )}
+        </ul>
+       </span>
+      </div>
+     </div>
+
+     <div className="header-block-info-section header-block-info-section-pdf">
       <span>
-       <SchoolIcon />
-       <Link href={DIPLOMA_LINK} text={t("university")} />
-      </span>
-      <span>
-       <TelegramIcon /> <Link href={TELEGRAM_LINK} text="Telegram" />
-       {/* <PhoneIcon /> <Link href={PHONE_LINK} text="+996558599499" /> */}
-      </span>
-      <span>
-       <MailIcon /> <Link href={EMAIL_LINK} text="yarsitdikov@gmail.com" />
-      </span>
-      <span>
-       <GitHubIcon /> <Link href={GITHUB_LINK} text="GitHub" />
-      </span>
-      <span>
-       <DownloadIcon /> <Link href={PDF_LINK(lang)} text="Pdf" />
-      </span>
-      <span className="header-block-info-links-languages">
-       <GlobeIcon />{" "}
-       <ul>
-        {(t("languages.compact", { returnObjects: true }) as string[]).map(
-         (lan: string, i: number) => (
-          <li key={i}>{lan}</li>
-         ),
-        )}
-       </ul>
+       <DownloadIcon />
+       <Link href={PDF_LINK(lang!)} text="PDF" />
       </span>
      </div>
     </div>
